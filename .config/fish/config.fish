f2cd2110fe83769994817d998875040e74619fb9
Kyler Wilson <kyler@viticusgroup.org>
2024-03-04 08:20:22 -0800

    fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fish_add_path /home/kylerw/.local/bin
+    set fish_greeting
 
     alias vim="nvim"
     alias ls="ll"
+
+    fish_add_path -g ~/.dotnet ~/.dotnet/tools
+    fish_add_path -g ~/.local/bin
+    fish_add_path -g /usr/local/go/bin
+    fish_add_path -g ~/.cargo/bin
+
+    set --universal nvm_default_version 18
 
     set -Ux EXA_STANDARD_OPTIONS --group --header --group-directories-first --all --icons
 
