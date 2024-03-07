83fb6b6f8cc7685eb138c3820cd63d41dcd43067
Kyler Wilson <kyler@viticusgroup.org>
2024-03-07 15:00:12 -0800

    pomodoro

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,6 +14,28 @@ if status is-interactive
 
     set -Ux EXA_STANDARD_OPTIONS --group --header --group-directories-first --all --icons
 
+    function pomo_options
+        switch $argv[1]
+        case 'work'
+            echo '45'
+        case 'break'
+            echo '10'
+        end
+    end
+
+    function pomodoro
+        set -l val $argv[1]
+
+        if test -n "$val" -a (pomo_options $val)
+            echo $val | lolcat
+            timer (pomo_options $val)m
+            spd-say "'$val' session done"
+        end
+    end
+
+    alias work="pomodoro 'work'"
+    alias br="pomodoro 'break'"
+
     starship init fish | source
     zoxide init --cmd cd fish | source
 end
