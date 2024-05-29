bbe158a72182bd7ce72650fe7684832659975374
Kyler Wilson <kyler.wilson.1@proton.me>
2024-05-28 18:53:45 -0700

    tmux

--- .config/fish/functions/l.fish
+++ .config/fish/functions/l.fish
@@ -1,4 +1,4 @@
-function l --wraps='eza $EXA_STANDARD_OPTIONS $EXA_L_OPTIONS' --wraps='exa $EXA_STANDARD_OPTIONS $EXA_L_OPTIONS' --description 'alias l exa $EXA_STANDARD_OPTIONS $EXA_L_OPTIONS'
+function l --wraps='exa $EXA_STANDARD_OPTIONS $EXA_L_OPTIONS' --description 'alias l exa $EXA_STANDARD_OPTIONS $EXA_L_OPTIONS'
   exa $EXA_STANDARD_OPTIONS $EXA_L_OPTIONS $argv
         
 end
