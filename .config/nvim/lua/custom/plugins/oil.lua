f0c4110b8b3e5e039539cc8459a2a53b61fbdd5c
Kyler Wilson <kyler@viticusgroup.org>
2024-06-18 08:52:17 -0700

    oil config

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -1,8 +1,20 @@
 return {
   'stevearc/oil.nvim',
+  opts = {},
+  config = function()
+    require('oil').setup {
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+        is_always_hidden = function(name, bufnr)
+          if name == '.git' or name == 'node_modules' or name == '.vscode' then
+            return true
+          end
+          return false
+        end,
+      },
+    }
 
-  opts = function()
-    require('oil').setup()
     vim.keymap.set('n', '<leader><CR>', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
   -- Optional dependencies
