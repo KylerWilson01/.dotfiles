4035642f1eb55169066d55edeff02fd9a11196ab
Kyler Wilson <kyler@viticusgroup.org>
2024-06-27 14:53:06 -0700

    dadbod

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,6 +27,24 @@ require('lazy').setup({
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
