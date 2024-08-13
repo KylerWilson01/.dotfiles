b949f87126497d261dfef631281daf91ef99eec6
Kyler Wilson <kyler@viticusgroup.org>
2024-08-12 17:05:25 -0700

    cmp keybind

--- .config/nvim/lua/custom/plugins/codeium.lua
+++ .config/nvim/lua/custom/plugins/codeium.lua
@@ -1,19 +1,10 @@
 return {
-  'Exafunction/codeium.vim',
-  event = 'BufEnter',
+  'Exafunction/codeium.nvim',
+  requires = {
+    'nvim-lua/plenary.nvim',
+    'hrsh7th/nvim-cmp',
+  },
   config = function()
-    vim.keymap.set('i', '<C-g>', function()
-      return vim.fn['codeium#Accept']()
-    end, { expr = true, silent = true, desc = 'Codeium Accept' })
-    vim.keymap.set('i', '<C-x>', function()
-      return vim.fn['codeium#Clear']()
-    end, { expr = true, silent = true, desc = 'Codeium Clear' })
-    vim.keymap.set('i', '<C-]>', function()
-      return vim.fn['codeium#CycleCompletions'](1)
-    end, { expr = true, silent = true, desc = 'Codeium Cycle Completions Next' })
-
-    vim.g.codeium_filetypes = {
-      markdown = false,
-    }
+    require('codeium').setup {}
   end,
 }
