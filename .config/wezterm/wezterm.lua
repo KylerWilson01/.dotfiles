cd84b01c27767a91fe7dfb23b75a53a32171410c
Kyler Wilson <kyler.wilson.1@proton.me>
2024-08-13 20:24:30 -0700

    wezterm starship i3

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.default_prog = { "/usr/bin/fish", "-l" }
+
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.window_background_opacity = 0.95
+config.font_size = 24.0
+
+config.enable_tab_bar = false
+config.enable_scroll_bar = false
+
+config.window_padding = {
+	left = "15%",
+	right = "15%",
+	top = 0,
+	bottom = 0,
+}
+
+return config
