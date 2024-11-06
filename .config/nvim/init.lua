02353595dca9437734f4b6281d345cf028e0d06a
Kyler Wilson <kyler@viticusgroup.org>
2024-11-05 16:29:53 -0800

    tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -277,7 +277,8 @@ require('nvim-treesitter.configs').setup {
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = true,
 
-  highlight = { enable = true },
+  highlight = { enable = true, additional_vim_regex_highlighting = false },
+
   indent = { enable = true },
   incremental_selection = {
     enable = true,
