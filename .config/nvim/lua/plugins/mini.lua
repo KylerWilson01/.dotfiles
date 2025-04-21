86fc2981f24061d82bb8fba9d47ebfc873f258b3
Kyler Wilson <kyler@viticusgroup.org>
2025-04-21 09:47:45 -0700

    Snacks
    - Notify
    - Pickers
    - Git
    - Lazy git

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -4,7 +4,6 @@ return {
   config = function()
     -- Appearance
     require('mini.icons').setup()
-    require('mini.notify').setup()
 
     -- Text Editing
     require('mini.ai').setup()
@@ -17,14 +16,10 @@ return {
     -- Workflow
     require('mini.bracketed').setup()
     require('mini.visits').setup()
-    require('mini.git').setup()
     require('mini.diff').setup()
-    require('mini.indentscope').setup()
-    local pick = require 'mini.pick'
     local extra = require 'mini.extra'
     local hipatterns = require 'mini.hipatterns'
 
-    pick.setup()
     extra.setup()
 
     local hi_words = extra.gen_highlighter.words
@@ -40,33 +35,8 @@ return {
         hex_color = hipatterns.gen_highlighter.hex_color(),
       },
     }
-
-    vim.keymap.set('n', '<leader>tgf', function()
-      pick.builtin.files { tool = 'rg' }
-    end, { desc = 'Search [G]it [F]iles' })
-
     vim.keymap.set('n', '<leader>tst', function()
       extra.pickers.hipatterns { { scope = 'all' } }
     end, { desc = '[S]earch [T]ags' })
-
-    vim.keymap.set('n', '<leader>tsf', function()
-      pick.builtin.files {}
-    end, { desc = '[S]earch [F]iles' })
-
-    vim.keymap.set('n', '<leader>tsh', function()
-      pick.builtin.help {}
-    end, { desc = '[S]earch [H]elp' })
-
-    vim.keymap.set('n', '<leader>tsg', function()
-      pick.builtin.grep_live { tool = 'rg' }
-    end, { desc = '[S]earch by [G]rep' })
-
-    vim.keymap.set('n', '<leader>tsd', function()
-      extra.pickers.diagnostic {}
-    end, { desc = '[S]earch [D]iagnostics' })
-
-    vim.keymap.set('n', '<leader><space>', function()
-      extra.pickers.explorer {}
-    end, { desc = '[ ] Find existing buffers' })
   end,
 }
---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,178 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    picker = { enabled = true, layout = { preset = 'telescope' } },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    statuscolumn = { enabled = true },
+    git = { enabled = true },
+    lazygit = { enabled = true },
+  },
+  keys = {
+    -- Lazy git
+    {
+      '<leader>gg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'Lazy Git',
+    },
+    -- Find
+    {
+      '<leader>fg',
+      function()
+        Snacks.picker.git_files()
+      end,
+      desc = 'Find Git Files',
+    },
+    {
+      '<leader>fc',
+      function()
+        Snacks.picker.files { cwd = vim.fn.stdpath 'config' }
+      end,
+      desc = 'Find Config File',
+    },
+    {
+      '<leader>ff',
+      function()
+        Snacks.picker.files()
+      end,
+      desc = 'Find Files',
+    },
+    {
+      '<leader>fp',
+      function()
+        Snacks.picker.projects()
+      end,
+      desc = 'Projects',
+    },
+    {
+      '<leader>fr',
+      function()
+        Snacks.picker.recent()
+      end,
+      desc = 'Recent',
+    },
+    {
+      '<leader>fr',
+      function()
+        Snacks.picker.recent()
+      end,
+      desc = 'Recent',
+    },
+    {
+      '<leader>ff',
+      function()
+        Snacks.picker.files()
+      end,
+      desc = 'Find Files',
+    },
+    -- search
+    {
+      '<leader>sd',
+      function()
+        Snacks.picker.diagnostics()
+      end,
+      desc = 'Diagnostics',
+    },
+    {
+      '<leader>sM',
+      function()
+        Snacks.picker.man()
+      end,
+      desc = 'Man Pages',
+    },
+    {
+      '<leader>sh',
+      function()
+        Snacks.picker.help()
+      end,
+      desc = 'Help Pages',
+    },
+    {
+      '<leader>sq',
+      function()
+        Snacks.picker.qflist()
+      end,
+      desc = 'Quickfix List',
+    },
+    -- Grep
+    {
+      '<leader>sg',
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = 'Grep',
+    },
+    {
+      '<leader>sw',
+      function()
+        Snacks.picker.grep_word()
+      end,
+      desc = 'Visual selection or word',
+      mode = { 'n', 'x' },
+    },
+    -- LSP
+    {
+      'gd',
+      function()
+        Snacks.picker.lsp_definitions()
+      end,
+      desc = 'Goto Definition',
+    },
+    {
+      'gD',
+      function()
+        Snacks.picker.lsp_declarations()
+      end,
+      desc = 'Goto Declaration',
+    },
+    {
+      'gr',
+      function()
+        Snacks.picker.lsp_references()
+      end,
+      nowait = true,
+      desc = 'References',
+    },
+    {
+      'gI',
+      function()
+        Snacks.picker.lsp_implementations()
+      end,
+      desc = 'Goto Implementation',
+    },
+    {
+      'gy',
+      function()
+        Snacks.picker.lsp_type_definitions()
+      end,
+      desc = 'Goto T[y]pe Definition',
+    },
+    {
+      '<leader>ss',
+      function()
+        Snacks.picker.lsp_symbols()
+      end,
+      desc = 'LSP Symbols',
+    },
+    {
+      '<leader>sS',
+      function()
+        Snacks.picker.lsp_workspace_symbols()
+      end,
+      desc = 'LSP Workspace Symbols',
+    },
+  },
+}
